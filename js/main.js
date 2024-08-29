ed3f1a2d1b51512cfdc9ed23841f5b7603485338
Ivan <vdeynesheko20@gmail.com>
2024-08-30 00:01:17 +0400

    8.15. Открывается и закрывается (часть 2) Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
 import {displayPictures} from './objects-miniatures/display-miniatures.js';
 import './fullsize-picture/display-fullsize-picture.js';
 displayPictures();
-
-
-
-//const array = [1,2,3,4,5,6,7,8,9,10];
-//let copyArray = Array.from(array);
-//const firstFiveNumbers = [1, 2, 3, 4, 5]
-//function firstFive (Array) {
-//  let newArray = [];
-//
-//  newArray.push(firstFiveNumbers)
-//  Array.splice(0, 5);
-//  return newArray;
-//}
-//
-//console.log(firstFive(copyArray), copyArray)
